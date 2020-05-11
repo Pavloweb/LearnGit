68d082a619e902cb486581f29051ccec1a68c2fd
Dima Morgunskiy <dimamorgunskiy@MacBook-Air-Dima.local>
2020-05-11 17:29:03 +0300

    Edit files

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Title</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <h1>Test git</h1>
+    </body>
+</html>
